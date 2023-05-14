d208cd00d359325e53a26f44067c55b158c49217
Robert Bedenbaugh <95264763+rhbedenbaugh@users.noreply.github.com>
2023-05-14 17:30:41 -0400

    additional state

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import CodeList from './CodeList';
 
-function Dropdown() {
+function Dropdown(CodeList) {
+  const [activeCode, setActiveCode] = useState();
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
         <InputLabel id='demo-simple-select-label'>Code</InputLabel>
         <Select
+          value={activeCode}
+          onChange={e => setActiveCode(e.target.value)}
           labelId='demo-simple-select-label'
           id='demo-simple-select'
           label='Code'
         >
-          <CodeList />
+          {options.map(o => (
+          <option key={o.value} value={o.value}>{o.label}</option>
+        ))}
+          {/* <CodeList /> */}
         </Select>
       </FormControl>
     </Box>
